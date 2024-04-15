30cf9be40113891035a7157a452c9ccf32f2a10c
annierose2007 <142624392+annierose2007@users.noreply.github.com>
2024-04-15 11:24:27 -0700

    Update APLine.java

--- src/main/java/APLine.java
+++ src/main/java/APLine.java
@@ -1,18 +1,14 @@
 public class APLine {
- /** State variables. Any numeric type; object or primitive. */
  private int a, b, c;
- /** Constructor with 3 int parameters. */
  public APLine(int a, int b, int c) {
- this.a = a;
- this.b = b;
- this.c = c;
+  this.a = a;
+  this.b = b;
+  this.c = c;
  }
- /** Determine the slope of this APLine. */
  public double getSlope() {
- return ( - (this.a / (double) this.b));
+  return ( - (this.a / (double) this.b));
  }
- /** Determine if coordinates represent a point on this APLine. */
  public boolean isOnLine(int x, int y) {
- return (0 == (this.a * x) + (this.b * y) + this.c);
+  return (0 == (this.a * x) + (this.b * y) + this.c);
  }
 }
